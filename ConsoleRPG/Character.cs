5ee7f7fa92bc7036905e3379168641129a6b755a
Tormie <mailforniels@gmail.com>
2022-03-29 11:50:03 +0200

    CharSystem
    Added groundwork for character expansion

--- ConsoleRPG/Character.cs
+++ ConsoleRPG/Character.cs
@@ -7,6 +7,22 @@ namespace ConsoleRPG
 {
     public class Character
     {
+        /*  Groundwork for expanded stats system for player and enemies
+         *  Strength manages melee damage
+         *  Agility manages to hit and/or dodge
+         *  Constitution manages hit points
+         *  Intelligence manages magic damage   */
+        public int strength;
+        public int agility;
+        public int constitution;
+        public int intelligence;
+        public int meleeDmgMod;
+        public int toHitMod;
+        public int dodgeMod;
+        public int hpMod;
+        public int magicDmgMod;
+
+
         public int hp;
         public int baseHP;
         public string name;
